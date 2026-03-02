bd1cde681ce9a37f745182e5e6b11722c0857136
Yahhweh <kameniv223@gmail.com>
2026-03-02 18:47:28 +0200

    init

---
+++ src/test/java/ibm/organisation/commenttotickettriage/CommentToTicketTriageApplicationTests.java
@@ -0,0 +1,13 @@
+package ibm.organisation.commenttotickettriage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentToTicketTriageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
